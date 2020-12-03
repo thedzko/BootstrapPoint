90eef04c95eac202181174509bbf5949b18218b5
thedzko <maumerk2@gmail.com>
2020-12-03 16:36:13 -0500

    Bootshit V1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var name = document.getElementById('email')
+
+$(".enviar").click(function enviarFormulario(){
+    if( email.value.length <= 0){
+        alert('Por favor, la información ;)')
+    }
+})
+
